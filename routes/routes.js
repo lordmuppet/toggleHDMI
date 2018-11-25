a87e52b3b312b169e656f7322a67e0452db7c423
Dave Yates <dyates@wisdomtree.com>
2018-11-25 15:56:12 -0500

    initial commit

---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+var appRouter = function (app) {
+    app.get("/off", function (req, res) {
+
+        const exec = require('child_process').exec;
+        var turnoff = exec('sh ~/rpi-hdmi.sh off',
+            (error, stdout, stderr) => {
+                console.log(`${stdout}`);
+                console.log(`${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+
+        res.status(200).send("Display turned off!");
+    });
+
+    app.get("/on", function (req, res) {
+
+        const exec = require('child_process').exec;
+        var turnoff = exec('sh ~/rpi-hdmi.sh on',
+            (error, stdout, stderr) => {
+                console.log(`${stdout}`);
+                console.log(`${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+        res.status(200).send("Display turned on!");
+    });
+}
+
+module.exports = appRouter;
