5753fd328cf179ed1f090bdc3378c97a991a3df5
Dave Yates <dyates@wisdomtree.com>
2025-08-05 19:06:52 -0400

    remove env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
     script: './index.js',            // or your entry .js
     cwd: '/home/pi/GitHubRepos/toggleHDMI',
     interpreter: '/home/pi/.nvm/versions/node/v22.16.0/bin/node',
-    env_file: '/home/pi/GitHubRepos/toggleHDMI/.env',
     env: {
       NODE_ENV: 'production',
       // any other vars you need…
